e7bad2a99ead9ceb1eaa0f4a4d0202942c9dd661
ScarHand420 <41939009+ScarHand420@users.noreply.github.com>
2019-02-26 07:34:22 -0500

    Parcial Por Carlos Morales
    ID: 000237516

--- Scripting_201910_Parcial1_base/Logic/Part.cs
+++ Scripting_201910_Parcial1_base/Logic/Part.cs
@@ -4,14 +4,37 @@
     {
         protected float speedBonus;
 
+        private float durability = 1;
+
         public int Level { get; protected set; }
         public abstract VehicleType Type { get; }
 
         public float SpeedBonus
         {
-            get { return 0F; }
+            get
+            {
+                //return 0F;
+                //return (Durability > 0) ? speedBonus * Durability : speedBonus;
+                if (speedBonus >= 0.5f)
+                {
+                    return speedBonus = 0.5f;
+                }
+                else
+                {
+                    if (Durability > 0)
+                    {
+                        return speedBonus = speedBonus * Durability;
+                    }
+                    else
+                    {
+                        return speedBonus;
+                    }
+                }
+            }
             protected set { speedBonus = value; }
         }
+
+        public float Durability { get => durability; /*set => durability = value;*/ }
 
         public Part()
         {
@@ -19,10 +42,16 @@
 
         public Part(float speedBonus)
         {
+            SpeedBonus = speedBonus;
         }
 
         public void Upgrade()
         {
+            if (Level <= 3)
+            {
+                Level++;
+                SpeedBonus = SpeedBonus + (SpeedBonus * 0.03f);
+            }
         }
     }
 }
--- Scripting_201910_Parcial1_base/Logic/Vehicle.cs
+++ Scripting_201910_Parcial1_base/Logic/Vehicle.cs
@@ -14,7 +14,24 @@
         {
             get
             {
-                return 0F;
+                //return 0F;
+                //return (CurrentPart == null && CurrentPart.SpeedBonus <= 0.5f) ? baseMaxSpeed : baseMaxSpeed + CurrentPart.SpeedBonus;
+                if (CurrentPart == null)
+                {
+                    return baseMaxSpeed;
+                }
+                else
+                {
+                    //if (CurrentPart.SpeedBonus > 0.5f)
+                    //{
+                    //    return baseMaxSpeed + CurrentPart.SpeedBonus;
+                    //}
+                    //else
+                    //{
+                    //    return baseMaxSpeed;
+                    //}
+                    return baseMaxSpeed += (baseMaxSpeed * CurrentPart.SpeedBonus);
+                }
             }
         }
 
@@ -35,6 +52,8 @@
 
             if (Type == part.Type || part.Type == VehicleType.Any)
             {
+                CurrentPart = part;
+                result = true;
             }
 
             return result;
@@ -42,6 +61,19 @@
 
         public void Upgrade()
         {
+            if (Level <= 3)
+            {
+                Level++;
+                baseMaxSpeed = baseMaxSpeed + (baseMaxSpeed * 0.05f);
+                if (CurrentPart != null)
+                {
+                    //Level++;
+                    //CurrentPart.SpeedBonus += CurrentPart.SpeedBonus * 0.03f;
+                    CurrentPart.Upgrade();
+                }
+            }
+            
+
         }
     }
 }
